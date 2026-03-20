b81aea18e8211b8aae2745cca6b9d26841a8314a
Soumodip Das <soumodipd567@gmail.com>
2026-03-20 14:10:18 +0530

    second commit

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const port = 4000
 
 // app.get('/', () => {}) // callbacks
 
+const data = {
+    name: "Mario",
+    age: 50,
+    location: "Kolkata",
+    marks: 95
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -20,6 +27,10 @@ app.get('/login', (req, res) => {
 
 app.get('/mario', (req, res) => {
   res.send('<h2>Welcome to Mario Website</h2>')
+})
+
+app.get('/data', (req, res) => {
+    res.json(data)
 })
 
 app.listen(process.env.PORT , () => {
