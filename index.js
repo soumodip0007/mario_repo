a2175f145d2527ac47a7114042408cf5a7cd70be
Soumodip Das <soumodipd567@gmail.com>
2026-03-20 14:44:28 +0530

    third commit

--- index.js
+++ index.js
@@ -34,5 +34,5 @@ app.get('/data', (req, res) => {
 })
 
 app.listen(process.env.PORT , () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
